201afcd26d96cba3ffb70c6d07a97d92a14bb9ca
Takayuki Komatsu <komatsutakayuki235@live.com>
2022-06-26 01:08:14 -0300

    chore: init pages and create HomeDrawer

---
+++ components/HomeDrawer.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-navigation-drawer permanent class="primary" dark app>
+    <v-list v-for="(link, i) in items" :key="i">
+      <v-list-item :to="link.to">
+        <v-list-item-icon>
+          <v-icon v-text="link.icon"></v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>{{ link.text }} </v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mini: false,
+      selectedItem: 0,
+      items: [
+        {
+          text: 'Eventos',
+          to: '/event',
+          icon: 'mdi-calendar',
+        },
+        {
+          text: 'Sala',
+          to: '/room',
+          icon: 'mdi-sofa-single-outline',
+        },
+        {
+          text: 'Sensores',
+          to: '/sensor',
+          icon: 'mdi-information',
+        },
+      ],
+    }
+  },
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -43,16 +43,16 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: process.env.VUE_APP_API,
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
-        dark: {
+        light: {
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
