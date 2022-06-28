e911ee7bc4615d872009ac680c22e2b450bf4685
Takayuki Komatsu <komatsutakayuki235@live.com>
2022-06-28 00:28:14 -0300

    feat: add v-calendar

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,64 @@
 <template>
-  <v-row justify="center" align="center"> Projeto Integrador 2: </v-row>
+  <v-calendar
+    ref="calendar"
+    v-model="value"
+    locale="pt-BR"
+    :type="type"
+    :events="events"
+    :event-overlap-mode="mode"
+    :event-overlap-threshold="30"
+    mode="stack"
+  ></v-calendar>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
   layout: 'default',
+
+  created() {
+    this.getEvents()
+  },
+  data() {
+    return {
+      value: '',
+      type: 'week',
+      mode: 'stack',
+      events: [],
+      colors: [
+        'blue',
+        'indigo',
+        'deep-purple',
+        'cyan',
+        'green',
+        'orange',
+        'grey darken-1',
+      ],
+    }
+  },
+  methods: {
+    rnd(a, b) {
+      return Math.floor((b - a + 1) * Math.random()) + a
+    },
+    async getEvents() {
+      const response = await this.$axios.get(`/events`)
+      const eventsInfo = response.data
+      let calendarValues = []
+
+      for (let count = 0; count < eventsInfo.length; count++) {
+        const event = eventsInfo[count]
+        const eventStart = `${event.diaEvent} ${event.horaInicio}`
+        const eventFinish = `${event.diaEvent} ${event.horaTermino}`
+
+        calendarValues.push({
+          name: null,
+          start: eventStart,
+          end: eventFinish,
+          color: this.colors[this.rnd(0, this.colors.length - 1)],
+        })
+      }
+      this.events = calendarValues
+    },
+  },
 }
 </script>
